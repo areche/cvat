e3a09461691a8949f757badec42f5191a83440ce
MARSS <marss@skynet.beaurivage>
2019-01-10 14:56:51 -0800

    Added start and stop scripts

---
+++ up.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f components/cuda/docker-compose.cuda.yml -f components/openvino/docker-compose.openvino.yml -f components/tf_annotation/docker-compose.tf_annotation.yml -f components/analytics/docker-compose.analytics.yml up -d
